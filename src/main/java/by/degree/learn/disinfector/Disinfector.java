417015323d967156e91159550dee6db944e7f552
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-11 22:17:36 +0200

    dumb implementation

--- src/main/java/by/degree/learn/disinfector/Disinfector.java
+++ src/main/java/by/degree/learn/disinfector/Disinfector.java
@@ -2,7 +2,10 @@ package by.degree.learn.disinfector;
 
 public class Disinfector {
     public void disinfect(Room room) {
+        System.out.println("Announce disinfection: leave " + room.getName());
+        System.out.println("Enforce!");
         System.out.println("Disinfecting root " + room.getName());
+        System.out.println("Announce disinfection complete");
     }
 
     public static void main(String[] args) {
