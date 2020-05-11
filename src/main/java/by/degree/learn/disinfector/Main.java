d5d11e68f5bf87aa8d509fb9c3c1bc95ecffc84b
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-11 23:17:56 +0200

    Support classes and interfaces in look up

--- src/main/java/by/degree/learn/disinfector/Main.java
+++ src/main/java/by/degree/learn/disinfector/Main.java
@@ -2,6 +2,7 @@ package by.degree.learn.disinfector;
 
 public class Main {
     public static void main(String[] args) {
-        new Disinfector().disinfect(new Room("laboratory"));
+        var disinfector = ObjectFactory.getInstance().create(Disinfector.class);
+        disinfector.disinfect(new Room("laboratory"));
     }
 }
--- src/main/java/by/degree/learn/disinfector/ObjectFactory.java
+++ src/main/java/by/degree/learn/disinfector/ObjectFactory.java
@@ -3,7 +3,6 @@ package by.degree.learn.disinfector;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Set;
 
 public class ObjectFactory {
     private static final ObjectFactory INSTANCE = new ObjectFactory();
@@ -13,18 +12,24 @@ public class ObjectFactory {
     }
 
     public <T> T create(Class<T> target) {
-        var reflections = new Reflections("by.degree.learn");
-        var implementors = reflections.getSubTypesOf(target);
+        Class<? extends T> implClass;
+        if (target.isInterface()) {
+            var reflections = new Reflections("by.degree.learn");
+            var implementors = reflections.getSubTypesOf(target);
 
-        // todo support Primary
-        if (implementors.size() != 1) {
-            throw new RuntimeException("Cannot instantiate " + target + ". It is implemented by " + implementors.size() + ". Must be exactly 1.");
+            // todo support Primary
+            if (implementors.size() != 1) {
+                throw new RuntimeException("Cannot instantiate " + target + ". It is implemented by " + implementors.size() + ". Must be exactly 1.");
+            }
+
+            implClass = implementors.iterator().next();
+        } else {
+            implClass = target;
         }
 
-        Class<? extends T> impl = implementors.iterator().next();
         try {
             // todo configure
-            return impl.getConstructor().newInstance();
+            return implClass.getConstructor().newInstance();
         } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
             throw new RuntimeException("Cannot instantiate ", e);
         }
