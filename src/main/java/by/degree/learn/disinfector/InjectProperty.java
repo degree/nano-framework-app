4ec7ab9c7b6b535b7567e4afe90b9fb8f9ef17b7
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-12 03:35:27 +0200

    Add target for annotations

--- src/main/java/by/degree/learn/disinfector/InjectProperty.java
+++ src/main/java/by/degree/learn/disinfector/InjectProperty.java
@@ -1,9 +1,12 @@
 package by.degree.learn.disinfector;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface InjectProperty {
     String value() default "";
 }
