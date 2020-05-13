8b46d36d4cb624b68889d70dc47cedb183f3f8f8
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-13 16:38:19 +0200

    remove anouncer proxy as component

--- src/main/java/by/degree/learn/disinfector/infrastructure/AnnouncerProxy.java
+++ src/main/java/by/degree/learn/disinfector/infrastructure/AnnouncerProxy.java
@@ -1,6 +1,5 @@
 package by.degree.learn.disinfector.infrastructure;
 
-import by.degree.learn.solid.framework.Component;
 import by.degree.learn.solid.framework.ProxyConfigurer;
 
 import java.lang.reflect.InvocationHandler;
@@ -9,7 +8,6 @@ import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-@Component
 public class AnnouncerProxy implements ProxyConfigurer {
     private static boolean hasAnnotation(Method method) {
         return method.isAnnotationPresent(Announce.class);
