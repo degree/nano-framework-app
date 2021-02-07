878076ea62586c33fba0f13d977837f5d012551d
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2021-02-07 02:05:25 +0100

    java 15 --preview

--- src/main/java/by/degree/learn/disinfector/model/Room.java
+++ src/main/java/by/degree/learn/disinfector/model/Room.java
@@ -1,16 +1,4 @@
 package by.degree.learn.disinfector.model;
 
-public class Room {
-    private final String name;
-
-    public Room(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+public record Room(String name) {
 }
